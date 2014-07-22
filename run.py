4ac747001905aa3ddde9b81ddb1d6fe0531e5e46
arcolife <arcshams@gmail.com>
2014-07-23 01:49:28 +0530

    add new routes

--- run.py
+++ run.py
@@ -11,17 +11,13 @@ import redis
 from os import chmod, \
                getpid, \
                getppid
-from shutil import copyfile
-from time import time
+
 from app import app
 
 from flask import Flask, \
     render_template, \
-    request, \
     Response, \
     json, \
-    Blueprint, \
-    flash, \
     g, \
     session, \
     redirect, \
@@ -30,7 +26,9 @@ from flask import Flask, \
 from config import \
     HOST, \
     PORT, \
-    DEBUG
+    DEBUG,\
+    TEMPLATE_CONFIGURATION
+
 
 @app.before_request
 def before_request():
@@ -44,7 +42,6 @@ def before_request():
         except: 
             g.user = 'anonymous'
 
-
 @app.route('/')
 def home(users=None):
     """
@@ -55,34 +52,20 @@ def home(users=None):
                            users=users,
                            **TEMPLATE_CONFIGURATION)
 
+@app.route('/agent')    
+def receive_call():
+    #resp = Response("""sent call generation request. <a href="/">Go back.</a>""")
+    return render_template('agent.html')
+
+@app.route('/customer')    
+def generate_call():
+    return render_template('customer.html')
 
 @app.route("/clouds.json")
 def clouds():
   data = app.redis.lrange("clouds", 0, -1)
   resp = Response(json.dumps(data), status=200, mimetype='application/json')
   return resp
-
-
-@app.route('/call')    
-def generate():
-    resp = Response("""sent call generation request. <a href="/">Go back.</a>""")
-    return resp
-    
-def save_pid():
-    """
-    Get the process ID and store it in a local file.
-    """
-    try:
-        pid = getpid()
-        ppid = getppid()
-        if ( ppid != 1 ):
-            pid = ppid
-        f = open(".pid", "w")
-        f.write(str(pid))
-        f.close()
-        chmod(".pid", 0600)
-    except:
-        pass
 
 
 if __name__ == '__main__':
