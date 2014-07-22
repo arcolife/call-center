cf4adea631d64032af5e5cf8901a98593a4d74af
arcolife <arcshams@gmail.com>
2014-07-22 18:57:50 +0530

    add wercker test case

---
+++ app-test.py
@@ -0,0 +1,28 @@
+from app import app
+import redis
+import os
+import unittest
+import json
+
+class CloudTestCase(unittest.TestCase):
+
+  def setUp(self):
+    app.redis.rpush('clouds','Altocumulus')
+    app.redis.rpush('clouds','Altostratus')
+    app.redis.rpush('clouds','Cumulonimbus')
+    app.redis.rpush('clouds','Nimbostratus')
+
+  def test_clouds(self):
+    tester = app.test_client(self)
+
+    response = tester.get('x/clouds.json', content_type='application/json')
+
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.data, json.dumps(['Altocumulus', 'Altostratus',
+      'Cumulonimbus', 'Nimbostratus']))
+
+  def tearDown(self):
+    app.redis.flushdb()
+
+if __name__ == '__main__':
+  unittest.main()
